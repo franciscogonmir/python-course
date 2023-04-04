9d66ee887abaab3cc8f15a1997678fd4060f6e4d
franciscogonzalezMiranda <franciscogonmirdeveloper@gmail.com>
2023-04-04 12:07:03 +0200

    refactor in funcion

--- exercise-5/exercise5.py
+++ exercise-5/exercise5.py
@@ -1,7 +1,9 @@
-isLeapYear = lambda year : (year % 400 == 0) and (year % 100 == 0) or (year % 4 ==0) and (year % 100 != 0)
-year = int(input("Enter year: "))
-if isLeapYear(year):
-    print(str(year) + " is leap year")
-else:
-    print(str(year) + " is not leap year")
+def getLeapYearInfo():
+    isLeapYear = lambda year : (year % 400 == 0) and (year % 100 == 0) or (year % 4 ==0) and (year % 100 != 0)
+    year = int(input("Enter year: "))
+    if isLeapYear(year):
+        return str(year) + " is leap year"
+    else:
+        return str(year) + " is not leap year"
 
+print(getLeapYearInfo())
