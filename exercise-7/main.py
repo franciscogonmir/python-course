03159014d4a67092adb858c14af45aa4f5b29be4
franciscogonzalezMiranda <franciscogonmirdeveloper@gmail.com>
2023-04-04 19:31:05 +0200

    refactor function operation

--- exercise-7/main.py
+++ exercise-7/main.py
@@ -2,14 +2,11 @@ import module.calculator as calculator
 
 
 def operations():
-    return "The result sum is {}" \
-           "\nThe result substraction is {}" \
-           "\nThe result multiply is {}" \
-           "\nThe result divide is {}".format(calculator.sum(2, 2)
-                                              ,calculator.subtraction(234, 453)
-                                              ,calculator.multiply(2, 2)
-                                              ,calculator.divide(16, 2))
+    sum = "{} + {} = {}".format(2, 2, calculator.sum(2, 2))
+    substr = "{} - {} = {}".format(234, 453, calculator.subtraction(234, 453))
+    multiply = "{} x {} = {}".format(2, 2, calculator.multiply(2, 2))
+    divide = "{} / {} = {}".format(16, 2, calculator.divide(16, 2))
+    return sum + "\n" + substr + "\n" + multiply + "\n" + divide
 
 
 print(operations())
-
